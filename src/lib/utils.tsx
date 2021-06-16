8a48447e4e85580fe7c6d7b0b1446b09edfbd4a1
Jordin Gardner <gard.jordin@gmail.com>
2021-06-16 01:05:25 -0700

    fix intermittent crash caused by mutating frameObj

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,30 +1,36 @@
 import { Fragment } from 'react';
 
 //Utility functions for ditto-react (Ditto and DittoProvider)
+//
 
 //returns block filtered by parameters provided in filters
 export const filterBlock = (blockObj, filters) => {
-  const block = Object.keys(blockObj).filter(textId => {
-    if (!filters || !filters.tags) return true;
-    return filters.tags.every(tag => (
-      blockObj[textId].tags &&
-      blockObj[textId].tags.includes(tag)
-    ));
-  }).reduce((obj, id) => {
-    obj[id] = blockObj[id].text;
-    return obj;
-  }, {});
-  return block;
+  return Object.keys(blockObj)
+    .filter(textId => {  
+      if (!filters?.tags) 
+        return true;
+
+      return (
+        filters.tags.every(tag => (
+          blockObj[textId].tags &&
+          blockObj[textId].tags.includes(tag)
+        ))
+      ) 
+    })
+    .reduce((obj, id) =>
+      ({ ...obj, [id]: blockObj[id].text }), {});
 }
 
-export const filterFrame = (frameObj, filters) => {
+export const filterFrame = (_frameObj, filters) => {
+  const frameObj = JSON.parse(JSON.stringify(_frameObj));
+
   if (frameObj.blocks) {
     for (var blockId in frameObj.blocks) {
       frameObj.blocks[blockId] = filterBlock(frameObj.blocks[blockId], filters);
     }
   }
-  frameObj.otherText = filterBlock(frameObj.otherText, filters);
-  return frameObj;
+
+  return { ...frameObj, otherText: filterBlock(frameObj.otherText, filters) };
 }
 
 export const error = (message: string, returnValue: any = message) => {
